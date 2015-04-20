d1c86dbd5d5eae3825f17d13b88d1aaac495586c
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-04-19 18:35:57 -0700

    uploaded files that were worked locally until completion

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # toDoList
+This is a simple todo list with some jQuery.
+
+If you click on the hamburger icon, a form appears to add items to the todo list.
+
+Current block: when I create a new item on the list, and check off the item (to indicate that I completed the item), the css doesn't change to show that it's done.
---
+++ main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	//add new item on todo list
+	$('#the_button').click(function(){
+		var input = $('input[name="todo"]').val()
+		$newItem = $('<li><input type="checkbox" class="completeItem">' + input + '</li>').addClass("highlight")
+		//does not allow items placed in list unless they have at least 1 character
+		if(input.length > 0) {
+			$('#theList').prepend($newItem);
+			// $newItem.removeClass("highlight", "500");
+			$('#todo').val('');
+			$newItem.removeClass("highlight", 1000); // highlight class never appears
+		}
+	});
+	//change class of items checked
+	$('.completeItem').click(function(){
+    	$(this).parent().toggleClass('complete');
+	});
+	//hides form within hamburger and shows when hamburger is clicked
+	$('form').hide();
+	$('.menu').click(function(){
+		$('form').show(1000);
+	});
+});
